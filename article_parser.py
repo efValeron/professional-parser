d6d73520b6f85d5b45e65e510dd9532117b47dc0
Valeriy <masterbox06@gmail.com>
2023-08-09 19:12:35 +0400

    yff

--- article_parser.py
+++ article_parser.py
@@ -1,25 +1,57 @@
-import sys, json, time, requests, os
+import json
+import os
+import re
+import sys
+import time
+
+import requests
+
 
 def remove_duplicates(arr1, arr2):
-    common_values = set(arr1) & set(arr2)
-    arr1 = [x for x in arr1 if x not in common_values]
-    arr2 = [x for x in arr2 if x not in common_values]
-    return arr1, arr2
+  common_values = set(arr1) & set(arr2)
+  arr1 = [x for x in arr1 if x not in common_values]
+  arr2 = [x for x in arr2 if x not in common_values]
+  return arr1, arr2
+
 
 def find_common_elements(arr1, arr2):
-    common_values = set(arr1) & set(arr2)
-    return list(common_values)
+  common_values = set(arr1) & set(arr2)
+  return list(common_values)
+
 
 def find_link_by_key(data, key):
-    for item in data:
-        if key in item:
-            return item[key]
-    return None
+  for item in data:
+    if key in item:
+      return item[key]
+  return None
+
+
+def parse_and_write_parts(parts):
+  for part in parts:
+    price_break_quantity_1 = ''
+    price_break_price_1 = ''
+    price_break_currency_1 = ''
+    
+    if 'PriceBreaks' in part and len(part["PriceBreaks"]) != 0:
+      pb = part['PriceBreaks'][0]
+
+      price_break_quantity_1 = str(pb.get('Quantity'))
+
+      price = pb.get('Price')
+      price_break_price_1 = re.search(r"\d+,?\d+", price).group()
+
+      price_break_currency_1 = pb.get('Currency')
+
+    with open('article_parse.csv', 'a') as kw_ps:
+      kw_ps.write(';'.join([
+        f"{part[value]}" if value in part else "" for value in values[:-3]
+      ]) + f";{price_break_quantity_1};{price_break_price_1};{price_break_currency_1}\n")
+
 
 if len(sys.argv) < 2:
-   print('Specify the path to the input file after main.exe\nThe program must be run from the command line!')
-   os.system('pause')
-   sys.exit()
+  print('Specify the path to the input file after main.exe\nThe program must be run from the command line!')
+  os.system('pause')
+  sys.exit()
 
 file_path = sys.argv[1]
 
@@ -29,12 +61,36 @@ headers = {
   'Content-Type': 'application/json',
   'Accept': 'application/json'
 }
+values = ['Availability',
+          'DataSheetUrl',
+          'Description',
+          'FactoryStock',
+          'ImagePath',
+          'Category',
+          'LeadTime',
+          'LifecycleStatus',
+          'Manufacturer',
+          'ManufacturerPartNumber',
+          'Min',
+          'Mult',
+          'MouserPartNumber',
+          'AlternatePackagings',
+          'ProductDetailUrl',
+          'Reeling',
+          'ROHSStatus',
+          'SuggestedReplacement',
+          'MultiSimBlue',
+          'AvailabilityInStock',
+          'Quantity',
+          'Price',
+          'Currency']
 
 with open(file_path, 'r') as f:
   file_rdln = f.readlines()
-  
+
   try:
-    articles = list(set([article.strip('\n').strip('.') for article in file_rdln if article.strip('\n').strip('.')]))
+    articles = list(
+      set([article.strip('\n').strip('.') for article in file_rdln if article.strip('\n').strip('.')]))
     if not articles:
       print('Input file is empty!')
       sys.exit()
@@ -45,11 +101,14 @@ with open(file_path, 'r') as f:
 
 grouped_articles = [articles[i:i + 10] for i in range(0, len(articles), 10)]
 
+with open('article_parse.csv', 'a') as kw_ps:
+  kw_ps.write(';'.join([value for value in values]) + '\n')
+
 limit_reached = False
 for index, articles_arr in enumerate(grouped_articles):
   time.sleep(2)
-  
-  #? debug
+
+  # ? debug
   print(f'PROCESSING {index + 1} REQUEST')
 
   if limit_reached:
@@ -74,13 +133,13 @@ for index, articles_arr in enumerate(grouped_articles):
     continue
   finally:
     if response.status_code != 200:
-      print('Request error') #? log error
+      print('Request error')  # ? log error
       for art in articles_arr:
         with open('article_parse.csv', 'a') as articles_parse:
           articles_parse.write(f'{art};request error\n')
       continue
     elif response.status_code == 403:
-      print('403 limit reached') #? log error
+      print('403 limit reached')  # ? log error
       for art in articles_arr:
         with open('article_parse.csv', 'a') as articles_parse:
           articles_parse.write(f'{art};limit reached\n')
@@ -95,7 +154,7 @@ for index, articles_arr in enumerate(grouped_articles):
         articles_parse.write(f'{art};')
         for error in json_res['Errors']:
           articles_parse.write(f'{error.get("Message")}')
-          print(error.get("Message")) #? log error
+          print(error.get("Message"))  # ? log error
         articles_parse.write('\n')
     continue
 
@@ -103,43 +162,53 @@ for index, articles_arr in enumerate(grouped_articles):
     for art in articles_arr:
       with open('article_parse.csv', 'a') as articles_parse:
         articles_parse.write(f'{art};request error\n')
-        print('SearchResult or Parts nof found') #? log error
+        print('SearchResult or Parts nof found')  # ? log error
     continue
   else:
-    response_parts = [
-      {part['MouserPartNumber']: part['ProductDetailUrl']}
-      if part['MouserPartNumber'] in articles_arr
-      else {part['ManufacturerPartNumber']: part['ProductDetailUrl']}
-      for part in json_res['SearchResults']['Parts']
-      if part['MouserPartNumber'] in articles_arr or part['ManufacturerPartNumber'] in articles_arr
-    ]
+    for part in json_res['SearchResults']['Parts']:
+      with open('article_parse.csv', 'a') as kw_ps:
+        kw_ps.write(';'.join([f"{part[value]}" if value in part else "" for value in values]) + '\n')
+      # print(part["Availability"] if "Availability" in part else "")
 
+    # response_parts = [
+    #   {part['MouserPartNumber']: part['ProductDetailUrl']}
+    #   if part['MouserPartNumber'] in articles_arr
+    #   else {part['ManufacturerPartNumber']: part['ProductDetailUrl']}
+    #   for part in json_res['SearchResults']['Parts']
+    #   if part['MouserPartNumber'] in articles_arr or part['ManufacturerPartNumber'] in articles_arr
+    # ]
 
-  
-  parts_keys = [list(part.keys())[0] for part in response_parts]
+    response_parts = json_res['SearchResults']['Parts']
 
-  not_defined_articles = remove_duplicates(parts_keys, articles_arr)[1]
-  defined_articles = find_common_elements(parts_keys, articles_arr)
+    print(response_parts[0]["PriceBreaks"])
 
-  try:
-    for defined_article in defined_articles:
-      with open('article_parse.csv', 'a') as articles_parse:
-        articles_parse.write(f'{defined_article};{find_link_by_key(response_parts, defined_article)}\n')
-  except Exception as err:
-    with open('article_parse.csv', 'a') as articles_parse:
-        articles_parse.write(f'{defined_article};writing error\n')
-        print('writing error') #? log error
-    continue
-  
-  try:
-    for not_defined_article in not_defined_articles:
-      with open('article_parse.csv', 'a') as articles_parse:
-        articles_parse.write(f'{not_defined_article};Not found\n')
-  except Exception as err:
-    with open('article_parse.csv', 'a') as articles_parse:
-        articles_parse.write(f'{defined_article};writing error\n')
-        print('writing error') #? log error
-    continue
-  
-  #? debug
-  print(f'{index + 1} REQUEST IS COMPLETE. {len(not_defined_articles)} ARTICLES NOT FOUND OUT OF {len(articles_arr)}')
+    parse_and_write_parts(response_parts)
+
+  # parts_keys = [list(part.keys())[0] for part in response_parts]
+
+  # not_defined_articles = remove_duplicates(parts_keys, articles_arr)[1]
+  # defined_articles = find_common_elements(parts_keys, articles_arr)
+
+  # try:
+  #   for defined_article in defined_articles:
+  #     with open('article_parse.csv', 'a') as articles_parse:
+  #       articles_parse.write(f'{defined_article};{find_link_by_key(response_parts, defined_article)}\n')
+  # except Exception as err:
+  #   with open('article_parse.csv', 'a') as articles_parse:
+  #       articles_parse.write(f'{defined_article};writing error\n')
+  #       print('writing error') #? log error
+  #   continue
+
+  # try:
+  #   for not_defined_article in not_defined_articles:
+  #     with open('article_parse.csv', 'a') as articles_parse:
+  #       articles_parse.write(f'{not_defined_article};Not found\n')
+  # except Exception as err:
+  #   with open('article_parse.csv', 'a') as articles_parse:
+  #       articles_parse.write(f'{defined_article};writing error\n')
+  #       print('writing error') #? log error
+  #   continue
+
+  # ? debug
+  # print(f'{index + 1} REQUEST IS COMPLETE. {len(not_defined_articles)} ARTICLES NOT FOUND OUT OF {len(articles_arr)}')
+  print(f'{index + 1} REQUEST IS COMPLETE')
--- keyword_parser.py
+++ keyword_parser.py
@@ -1,5 +1,6 @@
 import math
-import sys, json, time, requests, os
+import sys, json, time, requests, os, re
+
 
 def get_data(url, headers, data):
   try:
@@ -10,41 +11,69 @@ def get_data(url, headers, data):
     return None
   finally:
     if response.status_code != 200:
-      print('Request error') #? log error
+      print('Request error')  # ? log error
       with open('keywords_parse.csv', 'a') as kw_ps:
         kw_ps.write('request error' + '\n')
       return None
     elif response.status_code == 403:
-      print('403 limit reached') #? log error
+      print('403 limit reached')  # ? log error
       with open('keywords_parse.csv', 'a') as kw_ps:
         kw_ps.write('limit reached' + '\n')
       return 'limit_reached'
-  
+
   json_res = json.loads(response.text)
 
   if json_res['Errors']:
     with open('keywords_parse.csv', 'a') as kw_ps:
       for error in json_res['Errors']:
         kw_ps.write(f'{error.get("Message")}')
-        print(error.get("Message")) #? log error
+        print(error.get("Message"))  # ? log error
       kw_ps.write('\n')
     return None
-  
+
   if 'SearchResults' not in json_res or 'Parts' not in json_res['SearchResults']:
-    print('SearchResult or Parts nof found') #? log error
+    print('SearchResult or Parts nof found')  # ? log error
     return None
   elif 'SearchResults' not in json_res or 'NumberOfResult' not in json_res['SearchResults']:
-    print('NumberOfResult is not found') #? log error
+    print('NumberOfResult is not found')  # ? log error
     return None
   else:
     response_parts = json_res['SearchResults']['Parts']
     number_of_result = json_res['SearchResults']['NumberOfResult']
     return response_parts, number_of_result
 
+
+def parse_and_write_parts(parts):
+  for part in parts:
+    price_break_quantity_1 = ''
+    price_break_price_1 = ''
+    price_break_currency_1 = ''
+
+    # print(part['PriceBreaks'])
+    # print(part["MouserPartNumber"], part['PriceBreaks'][0])
+    # if part["MouserPartNumber"] == "792-MOKU-GO-M1-STM":
+    #   print(part["MouserPartNumber"], part["PriceBreaks"][0])
+
+    if 'PriceBreaks' in part and len(part["PriceBreaks"]) != 0:
+      pb = part['PriceBreaks'][0]
+
+      price_break_quantity_1 = str(pb.get('Quantity'))
+
+      price = pb.get('Price')
+      price_break_price_1 = re.search(r"\d+,?\d+", price).group()
+
+      price_break_currency_1 = pb.get('Currency')
+
+    with open('keywords_parse.csv', 'a') as kw_ps:
+      kw_ps.write(';'.join([
+        f"{part[value]}" if value in part else "" for value in values[:-3]
+      ]) + f";{price_break_quantity_1};{price_break_price_1};{price_break_currency_1}\n")
+
+
 if len(sys.argv) < 2:
-   print('Specify the path to the input file after main.exe\nThe program must be run from the command line!')
-   os.system('pause')
-   sys.exit()
+  print('Specify the path to the input file after main.exe\nThe program must be run from the command line!')
+  os.system('pause')
+  sys.exit()
 
 file_path = sys.argv[1]
 
@@ -73,14 +102,17 @@ values = ['Availability',
           'ROHSStatus',
           'SuggestedReplacement',
           'MultiSimBlue',
-          'AvailabilityInStock']
+          'AvailabilityInStock',
+          'Quantity',
+          'Price',
+          'Currency']
 
 with open('keywords_parse.csv', 'a') as kw_ps:
   kw_ps.write(';'.join([value for value in values]) + '\n')
 
 with open(file_path, 'r') as f:
   file_rdln = f.readlines()
-  
+
   try:
     keywords = [keyword.strip('\n') for keyword in file_rdln]
     if not keywords:
@@ -94,8 +126,8 @@ with open(file_path, 'r') as f:
 limit_reached = False
 for index, keyword in enumerate(keywords):
   time.sleep(2)
-  
-  #? debug
+
+  # ? debug
   print(f'PROCESSING {index + 1} REQUEST')
 
   if limit_reached:
@@ -122,9 +154,7 @@ for index, keyword in enumerate(keywords):
   else:
     response_parts, number_of_result = data[0], data[1]
 
-  for part_index, part in enumerate(response_parts):
-    with open('keywords_parse.csv', 'a') as kw_ps:
-      kw_ps.write(';'.join([f"{part[value]}" if value in part else "" for value in values]) + '\n')
+  parse_and_write_parts(response_parts)  # ?1111231
 
   for i in range(math.ceil(number_of_result / 50) - 1):
     starting_record = (i + 1) * 50 + 1
@@ -147,10 +177,7 @@ for index, keyword in enumerate(keywords):
     else:
       response_parts, number_of_result = data[0], data[1]
 
-    for part_index, part in enumerate(response_parts):
-      with open('keywords_parse.csv', 'a') as kw_ps:
-        kw_ps.write(';'.join([f"{part[value]}" if value in part else "" for value in values]) + '\n')
+    parse_and_write_parts(response_parts)  # ?1111231
 
-  
-  #? debug
+  # ? debug
   print(f'{index + 1} REQUEST IS COMPLETE.')
